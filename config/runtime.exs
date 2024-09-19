432538ed9bc5dec6233227d053fd065cb70f21e0
Andrew Chow <ac.andrewchow@gmail.com>
2024-09-19 12:51:38 -0400

    Refactors everything

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,6 +1,13 @@
 import Config
 
+if File.exists?(".env") do
+  File.stream!(".env")
+  |> Enum.each(fn line ->
+    [key, value] = String.split(line, "=", parts: 2)
+    System.put_env(String.trim(key), String.trim(value))
+  end)
+end
+
 config :spotify_playlist_generator,
-  client_id: "YOUR_CLIENT_ID",
-  client_secret: "YOUR_CLIENT_SECRET",
-  token_url: "https://accounts.spotify.com/api/token"
+  client_id: System.fetch_env!("CLIENT_ID"),
+  client_secret: System.fetch_env!("CLIENT_SECRET")
--- lib/spotify_playlist_generator.ex
+++ lib/spotify_playlist_generator.ex
@@ -1,96 +1,84 @@
 defmodule SpotifyPlaylistGenerator do
   @moduledoc """
-    Documentation for `SpotifyPlaylistGenerator`.
+  Documentation for `SpotifyPlaylistGenerator`.
 
-    iex > SpotifyPlaylistGenerator.create_playlist("track_id")
+  iex > SpotifyPlaylistGenerator.create_playlist("track_id")
   """
-  # https://accounts.spotify.com/authorize?client_id=YOUR_CLIENT_ID&response_type=token&redirect_uri=YOUR_CALLBACK_URI&scope=user-read-private%20playlist-read-private%20playlist-modify-public%20playlist-modify-private&state=YOUR_STATE
-  @token_url "https://accounts.spotify.com/api/token"
-  @auth_token ""
 
-  @doc """
-    Retrieves information for a track
-  """
+  @base_url "https://api.spotify.com/v1"
+  @auth_token "BQAR_S9PeSA_KtbDXYBqCmEnCkoNiEE4e210T3fTqN9kPuTD4T-hdv_nXU9NvtYxeO4qbjtn--hC8qLoJh_riubEYIPmRWT3HjKWkqmugOvcjL366K05KVJLXcvSMKUigMpbwKlnVgpsSMyAVXbc33ooPYH7ZMvpqFm4QMwRHZ-YewomNrS8MhZoIA038JPl5RmCk2GhpDItg-y0QPciEPG0PTSHK1dvujrICVIV4ctYV7TGCecLKSjXFN6LsSI18VM"
+  @default_limit 10
+  @default_seed_track "4kjI1gwQZRKNDkw1nI475M"
+
+  @spec get_env_configs() :: map()
+  def get_env_configs do
+    %{
+      client_id: System.fetch_env!("CLIENT_ID"),
+      client_secret: System.fetch_env!("CLIENT_SECRET")
+    }
+  end
+
+  @spec extract_auth_token(String.t()) :: String.t() | nil
+  def extract_auth_token(url) do
+    url
+    |> String.split("#")
+    |> List.last()
+    |> case do
+      nil ->
+        nil
+
+      fragment ->
+        fragment
+        |> String.split("&")
+        |> Enum.find_value(fn param ->
+          [key, value] = String.split(param, "=", parts: 2)
+          if key == "access_token", do: value
+        end)
+    end
+  end
+
+  @spec get_track_info(String.t()) :: {:ok, map()} | {:error, String.t()}
   def get_track_info(track_id) do
-    {:ok, access_token} = get_spotify_access_token()
-    url = "https://api.spotify.com/v1/tracks/#{track_id}"
-
-    headers = authorization_headers(access_token)
-
-    response = HTTPoison.get(url, headers)
-
-    case response do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, Jason.decode!(body)}
-
-      {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
-        {:error, "Unexpected status code: #{status_code}, body: #{body}"}
-
-      {:error, reason} ->
-        {:error, reason}
-    end
+    make_request("tracks/#{track_id}")
   end
 
   @doc """
-    Retrieves information for an album
+  Retrieves information for an album
   """
+  @spec get_album_info(String.t()) :: {:ok, map()} | {:error, String.t()}
   def get_album_info(album_id) do
-    url = "https://api.spotify.com/v1/albums/#{album_id}"
-
-    {:ok, access_token} = get_spotify_access_token()
-
-    headers = authorization_headers(access_token)
-
-    response = HTTPoison.get(url, headers)
-
-    case response do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, Jason.decode!(body)}
-
-      {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
-        {:error, "Unexpected status code: #{status_code}, body: #{body}"}
-
-      {:error, reason} ->
-        {:error, reason}
-    end
+    make_request("albums/#{album_id}")
   end
 
   @doc """
-    Generates a playlist of X songs related to X track
+  Generates a playlist of X songs related to X track
   """
-  def generate_playlist(playlist_name \\ "Recommended Playlist", opts \\ %{}) do
-    {:ok, access_token} = get_spotify_access_token()
+  @spec generate_playlist(String.t(), keyword()) :: {:ok, String.t()} | {:error, String.t()}
+  def generate_playlist(playlist_name \\ "Recommended Playlist", opts \\ []) do
+    opts = Keyword.merge([limit: @default_limit, seed_tracks: [@default_seed_track]], opts)
+    query_params = build_query_params(opts)
 
-    url = "https://api.spotify.com/v1/recommendations"
+    with {:ok, recommended_tracks} <- fetch_recommendations(query_params),
+         {:ok, playlist_data} <- create_playlist(playlist_name),
+         {:ok, _added_tracks} <- add_tracks_to_playlist(playlist_data["id"], recommended_tracks) do
+      {:ok, "Playlist created and tracks added successfully"}
+    end
+  end
 
-    headers = authorization_headers(access_token)
-
-    # Construct the query string based on a track for a recommendation
-    query_params = [
-      {"limit", Integer.to_string(Map.get(opts, :limit, 100))},
-      {"seed_tracks", Enum.join(Map.get(opts, :seed_tracks, ["4kjI1gwQZRKNDkw1nI475M"]), ",")}
+  defp build_query_params(opts) do
+    [
+      {"limit", Integer.to_string(opts[:limit])},
+      {"seed_tracks", Enum.join(opts[:seed_tracks], ",")}
     ]
+    |> URI.encode_query()
+  end
 
-    url_with_query = "#{url}?#{URI.encode_query(query_params)}"
+  defp fetch_recommendations(query_params) do
+    url = "#{@base_url}/recommendations?#{query_params}"
 
-    response = HTTPoison.get(url_with_query, headers)
-
-    case response do
+    case HTTPoison.get(url, authorization_headers()) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        recommended_tracks = Jason.decode!(body)["tracks"]
-
-        {:ok, playlist_data} = create_playlist(playlist_name)
-
-        playlist_map = Jason.decode!(playlist_data)
-
-        IO.inspect(playlist_map)
-
-        {:ok, _added_tracks} =
-          add_tracks_to_playlist(
-            Map.get(playlist_map, "id"),
-            recommended_tracks,
-            @auth_token
-          )
+        {:ok, Jason.decode!(body)["tracks"]}
 
       {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
         {:error, "Unexpected status code: #{status_code}, body: #{body}"}
@@ -101,9 +89,7 @@ defmodule SpotifyPlaylistGenerator do
   end
 
   defp create_playlist(playlist_name) do
-    url = "https://api.spotify.com/v1/me/playlists"
-
-    headers = authorization_headers(@auth_token)
+    url = "#{@base_url}/me/playlists"
 
     body_params = %{
       name: playlist_name,
@@ -111,13 +97,9 @@ defmodule SpotifyPlaylistGenerator do
       public: true
     }
 
-    response = HTTPoison.post(url, Jason.encode!(body_params), headers)
-
-    IO.inspect(response)
-
-    case response do
+    case HTTPoison.post(url, Jason.encode!(body_params), authorization_headers()) do
       {:ok, %HTTPoison.Response{status_code: 201, body: body}} ->
-        {:ok, body}
+        {:ok, Jason.decode!(body)}
 
       {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
         {:error, "Unexpected status code: #{status_code}, body: #{body}"}
@@ -127,50 +109,36 @@ defmodule SpotifyPlaylistGenerator do
     end
   end
 
-  defp authorization_headers(access_token) do
-    [
-      {"Authorization", "Bearer #{access_token}"},
-      {"Content-Type", "application/json"}
-    ]
-  end
+  defp add_tracks_to_playlist(playlist_id, tracks) do
+    url = "#{@base_url}/playlists/#{playlist_id}/tracks"
+    track_uris = Enum.map(tracks, &("spotify:track:" <> &1["id"]))
+    body_params = %{uris: track_uris}
 
-  defp get_spotify_access_token do
-    body_params = [
-      grant_type: "client_credentials",
-      client_id: Application.get_env(:spotify_playlist_generator, :client_id),
-      client_secret: Application.get_env(:spotify_playlist_generator, :client_secret)
-    ]
+    case HTTPoison.post(url, Jason.encode!(body_params), authorization_headers()) do
+      {:ok, %HTTPoison.Response{status_code: 201}} ->
+        {:ok, "Tracks added to playlist successfully"}
 
-    headers = ["Content-Type": "application/x-www-form-urlencoded"]
-
-    response = HTTPoison.post(@token_url, URI.encode_query(body_params), headers)
-
-    case response do
-      {:ok, %HTTPoison.Response{body: body}} ->
-        {:ok, Jason.decode!(body)["access_token"]}
+      {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
+        {:error, "Unexpected status code: #{status_code}, body: #{body}"}
 
       {:error, reason} ->
         {:error, reason}
     end
   end
 
-  defp add_tracks_to_playlist(playlist_id, tracks, access_token) do
-    url = "https://api.spotify.com/v1/playlists/#{playlist_id}/tracks"
-    headers = authorization_headers(access_token)
+  defp authorization_headers do
+    [
+      {"Authorization", "Bearer #{@auth_token}"},
+      {"Content-Type", "application/json"}
+    ]
+  end
 
-    IO.inspect(playlist_id)
+  defp make_request(endpoint) do
+    url = "#{@base_url}/#{endpoint}"
 
-    track_uris = Enum.map(tracks, &("spotify:track:" <> &1["id"]))
-
-    body_params = %{
-      uris: track_uris
-    }
-
-    response = HTTPoison.post(url, Jason.encode!(body_params), headers)
-
-    case response do
-      {:ok, %HTTPoison.Response{status_code: 201, body: _body}} ->
-        {:ok, "Tracks added to playlist successfully"}
+    case HTTPoison.get(url, authorization_headers()) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, Jason.decode!(body)}
 
       {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
         {:error, "Unexpected status code: #{status_code}, body: #{body}"}
